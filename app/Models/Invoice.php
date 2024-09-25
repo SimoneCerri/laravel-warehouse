88f517d2a6bb5984baa6be1b0e888dd58975a9ae
SimoneCerri <simone.cerri1994@gmail.com>
2024-09-25 17:08:30 +0200

    relationship between invoice and order

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Invoice extends Model
 {
     use HasFactory;
+    protected $fillable = ['invoice_date', 'total', 'order_id'];
+
+    /**
+     * Get the order that owns the invoice
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function order(): BelongsTo
+    {
+        return $this->belongsTo(Order::class,);
+    }
 }
--- app/Models/Order.php
+++ app/Models/Order.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Order extends Model
 {
@@ -19,5 +20,15 @@ class Order extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class,);
+    }
+
+    /**
+     * Get the invoice associated with the Order
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function invoice(): HasOne
+    {
+        return $this->hasOne(Invoice::class,);
     }
 }
